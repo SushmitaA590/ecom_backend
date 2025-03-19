4e426fee6cf0d7d5d99bffeecef5bdb2007e79a9
SushmitaA590 <sushmitaagasibagil@gmail.com>
2025-03-19 15:48:16 +0530

    third

--- server.js
+++ server.js
@@ -5,9 +5,19 @@ const router = require('./routes/authRoutes');
 const cartrouter = require('./routes/cartRoutes');
 
 const app = express();
+const allowedOrigins =["ecom-frontend-o501a99cn-sushmita-c-as-projects.vercel.app" , "ecom-frontend-pied.vercel.app"
+]
 
-
-app.use(cors())
+app.use(cors({
+origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true, // Allows cookies and authentication headers if needed
+}))
 app.use(express.json())
 
 connectDB()
